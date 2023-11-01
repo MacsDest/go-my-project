539173d0957620595160cebb96c36717a9af7ef2
HJM <519523287@qq.com>
2023-11-01 20:59:51 +0800

    20231101

--- cmd/main.go
+++ cmd/main.go
@@ -8,5 +8,15 @@ import (
 
 func main() {
 	my_app01.Sum()
-	fmt.Println(math.MinInt)
+	fmt.Println(math.MinInt8)
+	arr := [10]int{}
+	fmt.Println(arr)
+	arr1 := arr
+	arr1[7] = 99
+	fmt.Println(arr1)
+	fmt.Println(arr)
+	a := [...]int{99: 1}
+	var p *[100]int = &a
+	fmt.Println(*p)
+
 }
